f35a4a508f86187d9a4db7aff0fa7c238a7d5e85
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-11-07 15:53:56 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,18 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+ list_of_alternating_signs = []
+ for i in range(n_terms):
+  if i % 2 == 0 :
+    list_of_alternating_signs.append(1)
+  else :
+    list_of_alternating_signs.append(-1)
+ leibniz_series = []
+ for i in range(n_terms) :
+  sign = list_of_alternating_signs[i]
+  num = (1/(2*i + 1)) * sign
+  leibniz_series.append(num)
+ total = sum( leibniz_series )
+ pie = total*4
+ return pie
+
+
+ 
